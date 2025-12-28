71e89fa4920968ff713a8dd7a933ffd9e1e5aa00
Aliz <ar.mokhtari.g@gmail.com>
2025-12-28 10:36:47 +0330

    fix(config): 127.0.0.1 instead of localhost

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // src/config.ts
 export const APP_CONFIG = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1',
-    wsUrl: process.env.REACT_APP_WS_URL || 'ws://localhost:8080/ws',
+    apiUrl: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8080/api/v1',
+    wsUrl: process.env.REACT_APP_WS_URL || 'ws://127.0.0.1:8080/ws',
 };
